f842067aacac45e0d63e070a610fd9b761d675dd
wangqz <2809755637@qq.com>
2023-02-06 17:03:52 +0800

    add config

--- .dumirc.ts
+++ .dumirc.ts
@@ -6,6 +6,9 @@ export default defineConfig({
     logo: '/images/logo.png',
     name: 'wz-doc',
     footer: '豫ICP备2022007076号-1 豫ICP备2022007076号',
+    socialLinks: {
+      github: 'https://github.com/wangz-code',
+    },
   },
   favicons: ['/images/favicon.png'],
 });
