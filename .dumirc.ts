faaef04613809ec009715526939910d6ce7dff27
wangqz <2809755637@qq.com>
2023-02-07 15:42:42 +0800

    增加keywords

--- .dumirc.ts
+++ .dumirc.ts
@@ -10,5 +10,9 @@ export default defineConfig({
       github: 'https://github.com/wangz-code',
     },
   },
+  metas:[
+    { name: 'keywords', content: 'nunu,js,javascript,蓝牙小程序,服务端,数据库,git,go,nodejs,输入法,ffmpeg视频处理,爬虫' },
+    { name: 'description', content: '个人在前端开发后端工作中遇到的经验碎碎念,balbala 之类的东西' },
+  ],
   favicons: ['/images/favicon.png'],
 });
--- docs/index.md
+++ docs/index.md
@@ -1,8 +1,5 @@
 ---
 title: 首页
-description: 这是一个个人站点,js,git
-keywords: ["nodejs","ffmpeg","git","blog","dumi"]
-footer: Open-source MIT Licensed | Copyright © 2019-present<br />Powered by self
 ---
 
 <code src="./index.jsx" inline > </code>
