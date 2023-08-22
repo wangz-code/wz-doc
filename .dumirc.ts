3ea353ca3b5899fd0f80ca67447684a270b2e501
wangqz <2809755637@qq.com>
2023-08-22 18:14:12 +0800

    买新电脑啦啦啦啦

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,14 +5,22 @@ export default defineConfig({
   themeConfig: {
     logo: '/images/logo.png',
     name: 'wz-doc',
-    footer: '<a style="margin-right:10px" href="https://beian.miit.gov.cn" target="_blank">豫ICP备2022007076号</a>  <a style="margin-right:10px" href="https://beian.miit.gov.cn">豫ICP备2022007076号-1</a> <a href="/other/disclaimer">免责声明</a>   ',
+    footer:
+      '<a style="margin-right:10px" href="https://beian.miit.gov.cn" target="_blank">豫ICP备2022007076号</a>  <a style="margin-right:10px" href="https://beian.miit.gov.cn">豫ICP备2022007076号-1</a> <a href="/other/disclaimer">免责声明</a>   ',
     socialLinks: {
       github: 'https://github.com/wangz-code',
     },
   },
-  metas:[
-    { name: 'keywords', content: 'nunu,js,javascript,蓝牙小程序,服务端,数据库,git,go,nodejs,输入法,ffmpeg视频处理,爬虫' },
-    { name: 'description', content: '前端,后端,爬虫,蓝牙,小程序开发后端工作中遇到的经验碎碎念,balbala 之类的东西' },
+  metas: [
+    {
+      name: 'keywords',
+      content:
+        'js,javascript,小程序,服务端,数据库,git,go,nodejs,输入法,ffmpeg,折腾日记,电脑,游戏',
+    },
+    {
+      name: 'description',
+      content: '前端|后端|蓝牙|小程序日常折腾|技术笔记|电脑装机|ai绘图',
+    },
   ],
   favicons: ['/images/favicon.png'],
 });
